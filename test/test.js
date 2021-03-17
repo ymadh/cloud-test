e9a3f4b752f75619883a7312f5c952aa1ebd1e42
Amy Wightman <amy.wightman@quest.com>
2021-03-17 14:04:49 -0400

    adding test directory

---
+++ test/test.js
@@ -0,0 +1,19 @@
+process.env.NODE_ENV = 'test';
+
+var expect = require('chai').expect;
+let chaiHttp = require('chai-http');
+let chai = require('chai');
+let server = require('../index');
+chai.use(chaiHttp);
+
+
+describe('/GET homepage', () => {
+    it('it should GET the home page', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                chai.expect(res.text).equal('Well hello #3')
+                done();
+            });
+    });
+});
